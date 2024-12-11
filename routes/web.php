6f620e014a2f7a1afd89ba9693dc16f23910fa5d
Alex_dev <131791659+SpeedAlex150@users.noreply.github.com>
2024-12-11 15:47:32 +0100

    Instalamos Livewire

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,13 @@ Route::get('/', PageHomeController::class)->name('pages.home');
 
 Route::get('courses/{course:slug}', PageCourseDetailsController::class)
     ->name('pages.course-details');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
